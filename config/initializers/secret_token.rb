df73cfc3482443b4dbe73eefc59a1ddb8580d391
Michael Ball <mwball@gmail.com>
2011-08-18 11:41:55 -0400

    cincy cloud initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CincyCloud::Application.config.secret_token = '99212284f9f764fa2b1de0cbdfd76f630bee21e91ec9a02346767a746f16edf21eb768718baf94767e4e2aec9dace9e8bb2e782c74d776a46afa129327ef5a6f'
