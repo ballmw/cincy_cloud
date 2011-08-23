3e2b3cbaf67d240cf5b3c6806737662d9d1f9624
Michael Ball <mwball@gmail.com>
2011-08-23 15:38:48 -0400

    added theming etc

--- Gemfile
+++ Gemfile
@@ -46,14 +46,20 @@ end
 
 # USER DEFINED
 
+gem "jquery-rails"
 
 # Specify additional Refinery CMS Engines here (all optional):
-# gem 'refinerycms-inquiries',    '~> 1.0'
+gem 'refinerycms-inquiries',    '~> 1.0'
 # gem "refinerycms-news",         '~> 1.2'
-# gem 'refinerycms-blog',         '~> 1.6'
+gem 'refinerycms-blog',         '~> 1.6.1'
 # gem 'refinerycms-page-images',  '~> 1.0'
+gem 'refinerycms-search', '~> 0.9.8'
+gem 'refinerycms-calendar', '~>1.1.0'
+
 
 # Add i18n support (optional, you can remove this if you really want to).
 gem 'refinerycms-i18n',         '~> 1.0.0'
 
 # END USER DEFINED
+gem "twitter", "~> 1.6.2"
+gem 'refinerycms-twitter_streams', '1.0', :path => 'vendor/engines'
---
+++ public/javascripts/application.js
@@ -0,0 +1,51 @@
+$(function(){
+var twitter = {
+    container: $('#tweets_container'),
+    loading_image: $('#twitter_loading'),
+    update_interval: 420000, //7 minutes
+    clearTimeout: function() {
+        if (this.timer_id) {
+            clearTimeout(this.timer_id)
+        }
+    },
+    autoUpdate: function() {
+        var self = this;
+        self.clearTimeout();
+        self.timer_id = setTimeout(function() {
+            self.initialize();
+        }, self.update_interval);
+    },
+    initialize: function() {
+        var self = this;
+        self.clearTimeout();
+        if ($('#tweets_container').size() > 0) {
+            $.ajax({
+                type: 'get',
+                url: "/twitter_streams/1",
+                data: {},
+                dataType: 'html',
+                ifModified: true,
+                beforeSend: function() {
+                    self.container.hide('slow');
+                    self.loading_image.show('fast');
+                },
+                success: function(data, status) {
+                    if (status != 'notmodified') {
+                        self.container.fadeOut(1200, function() {
+                            $(this).html(data);
+                            $(this).fadeIn(1200);
+                        });
+                    }
+                    self.loading_image.hide('fast');
+                    self.autoUpdate();
+                },
+                error: function() {
+                    self.loading_image.hide('fast');
+                    //self.autoUpdate();
+                }
+            });
+        }
+    }
+};
+twitter.initialize();
+});
