f768680067656eae391ff910f0e426271d818bc4
Michael Ball <mwball@gmail.com>
2011-08-23 16:14:51 -0400

    add inquiries plugin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110823181657) do
+ActiveRecord::Schema.define(:version => 20110823201429) do
 
   create_table "blog_categories", :force => true do |t|
     t.string   "title"
@@ -101,6 +101,26 @@ ActiveRecord::Schema.define(:version => 20110823181657) do
     t.datetime "updated_at"
     t.string   "image_uid"
     t.string   "image_ext"
+  end
+
+  create_table "inquiries", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "spam",       :default => false
+  end
+
+  add_index "inquiries", ["id"], :name => "index_inquiries_on_id"
+
+  create_table "inquiry_settings", :force => true do |t|
+    t.string   "name"
+    t.text     "value"
+    t.boolean  "destroyable"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "page_part_translations", :force => true do |t|
